cbb27daef0835e6d9ed9162293d21b44ac224f2f
Arlyn <arlyn.goodrich@gmail.com>
2022-01-09 11:58:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectDemeter
+
+Developed with Unreal Engine 4
---
+++ Source/ProjectDemeter/ProjectDemeterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectDemeterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEMETER_API AProjectDemeterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
