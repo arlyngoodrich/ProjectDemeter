cf00fca880bf302eb660aead4061bc4d041192e0
Arlyn <arlyn.goodrich@gmail.com>
2022-01-13 08:22:16 -0600

    DEV-89 #comment created custom character movement component

---
+++ Source/ProjectDemeter/Private/CharacterSystem/PlayerCharMovementComponent_C.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterSystem/PlayerCharMovementComponent_C.h"
+
---
+++ Source/ProjectDemeter/Public/CharacterSystem/PlayerCharMovementComponent_C.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PlayerCharMovementComponent_C.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEMETER_API UPlayerCharMovementComponent_C : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
