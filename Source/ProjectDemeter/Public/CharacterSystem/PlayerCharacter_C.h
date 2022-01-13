92e6aeac6f95b5f74a156db8658c193ca0b81d6e
Arlyn <arlyn.goodrich@gmail.com>
2022-01-13 08:17:10 -0600

    DEV-82 #comment created class

---
+++ Source/ProjectDemeter/Public/CharacterSystem/PlayerCharacter_C.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterSystem/Character_C.h"
+#include "PlayerCharacter_C.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEMETER_API APlayerCharacter_C : public ACharacter_C
+{
+	GENERATED_BODY()
+	
+};
