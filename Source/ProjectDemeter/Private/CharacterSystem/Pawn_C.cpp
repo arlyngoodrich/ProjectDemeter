5a34dd235d8db66ba85cddd33c50d9fcaa39f63f
Arlyn <arlyn.goodrich@gmail.com>
2022-01-13 08:06:53 -0600

    DEV-88 #comment created custom pawn.  #Timetracking 5m

---
+++ Source/ProjectDemeter/Private/CharacterSystem/Pawn_C.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterSystem/Pawn_C.h"
+
+// Sets default values
+APawn_C::APawn_C()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APawn_C::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APawn_C::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APawn_C::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
