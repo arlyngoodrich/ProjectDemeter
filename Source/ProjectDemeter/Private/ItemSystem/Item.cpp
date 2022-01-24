10a929fa6f01268b734e290e31165ad5cec149a5
Arlyn <arlyn.goodrich@gmail.com>
2022-01-24 16:53:26 -0600

    DEV-215 Made default item class replicate

--- Source/ProjectDemeter/Private/ItemSystem/Item.cpp
+++ Source/ProjectDemeter/Private/ItemSystem/Item.cpp
@@ -12,6 +12,7 @@ AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SetReplicates(true);
 	InteractionComponent = CreateDefaultSubobject<UInteractableObjectComponent>(TEXT("Interaction Component"));
 }
 
