649c53fcb749c951e54a4732f49bdb5e9671d762
Arlyn <arlyn.goodrich@gmail.com>
2022-01-24 17:50:31 -0600

    Revert "Dev 170 inventory component"

--- Source/ProjectDemeter/Private/InteractionSystem/InteractableObjectComponent.cpp
+++ Source/ProjectDemeter/Private/InteractionSystem/InteractableObjectComponent.cpp
@@ -4,8 +4,6 @@
 #include "InteractionSystem/InteractableObjectComponent.h"
 #include "Core/Logs_C.h"
 
-//UE4 Includes
-#include "Components/MeshComponent.h"
 
 // Sets default values for this component's properties
 UInteractableObjectComponent::UInteractableObjectComponent()
@@ -13,28 +11,17 @@ UInteractableObjectComponent::UInteractableObjectComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-	bShouldOutline = true;
-	OutlineStencilValue = 1;
 
 	// ...
 }
 
 
-void UInteractableObjectComponent::Interact(AActor* InstigatingActor)
+void UInteractableObjectComponent::Interact()
 {
-
-	if (!InstigatingActor)
-	{
-		UE_LOG(LogInteractionSystem, Error, TEXT("Interaction triggered on %s by null instigator"), *GetOwner()->GetName())
-		return;
-	}
-
 	if (GetOwnerRole() == ROLE_Authority)
 	{
 		UE_LOG(LogInteractionSystem, Log, TEXT("Interaction called on %s"), *GetOwner()->GetName())
-
-		BP_OnInteractionTriggered(InstigatingActor);
-		OnInteractionTriggered.Broadcast(InstigatingActor);
+		BP_OnInteractionTriggered();
 	}
 	else
 	{
@@ -51,23 +38,11 @@ void UInteractableObjectComponent::ToggleFocus(bool bNewIsInFocus)
 	{
 		UE_LOG(LogInteractionSystem, Log, TEXT("%s is now in focus"), *GetOwner()->GetName())
 		BP_OnStartFocus();
-		
-		if (bShouldOutline)
-		{
-			ToggleOutline(true);
-		}
-
 	}
 	else
 	{
 		UE_LOG(LogInteractionSystem, Log, TEXT("%s is no longer in focus"), *GetOwner()->GetName())
 		BP_OnEndFocus();
-
-		if (bShouldOutline)
-		{
-			ToggleOutline(false);
-		}
-
 	}
 
 	
@@ -82,19 +57,5 @@ void UInteractableObjectComponent::BeginPlay()
 	// ...
 	
 }
-
-void UInteractableObjectComponent::ToggleOutline(bool bStartOutline)
-{
-	TArray<UMeshComponent*> MeshComponents;
-	GetOwner()->GetComponents<UMeshComponent>(MeshComponents);
-
-	for (int i = 0; i < MeshComponents.Num(); i++)
-	{
-		MeshComponents[i]->SetRenderCustomDepth(bStartOutline);
-		MeshComponents[i]->SetCustomDepthStencilValue(OutlineStencilValue);
-	}
-}
-
-
 
 
