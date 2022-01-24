dcdfb1969c1e7732c9f7546d2266bef84e9c2179
Arlyn <arlyn.goodrich@gmail.com>
2022-01-24 14:47:06 -0600

    DEV-186 Created Item Data

---
+++ Source/ProjectDemeter/Public/ItemSystem/ItemData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Info")
+	FName DisplayName;
+
+};
