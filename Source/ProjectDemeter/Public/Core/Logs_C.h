7d9af19d59275ee18c8d1aeab13cea5e59ce5666
Arlyn <arlyn.goodrich@gmail.com>
2022-01-26 12:48:23 -0600

    DEV-211 Added Custom Attribute Log

--- Source/ProjectDemeter/Public/Core/Logs_C.h
+++ Source/ProjectDemeter/Public/Core/Logs_C.h
@@ -8,6 +8,7 @@ DECLARE_LOG_CATEGORY_EXTERN(LogStamina, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogHealth, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogInteractionSystem, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogInventorySystem, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogAttributeSystem, Log, All);
 
 
 class PROJECTDEMETER_API Logs_C;
