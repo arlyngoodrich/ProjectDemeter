682892b4d36058ad7e74b84bcf3cca868ae4ba1a
Arlyn <arlyn.goodrich@gmail.com>
2022-02-07 12:06:28 -0600

    DEV-256 Added New Inventory Classes

---
+++ Source/ProjectDemeter/Public/ItemSystem/PlayerInventory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemSystem/InventoryComponent.h"
+#include "PlayerInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEMETER_API UPlayerInventory : public UInventoryComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectDemeter/Public/ItemSystem/StorageInventory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemSystem/InventoryComponent.h"
+#include "StorageInventory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDEMETER_API UStorageInventory : public UInventoryComponent
+{
+	GENERATED_BODY()
+	
+};
